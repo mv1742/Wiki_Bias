06f3bba9c69c0d2f1765176025fdbca3e507a647
Manrique Vargas <43248948+mv1742@users.noreply.github.com>
2019-10-27 00:34:15 -0400

    Update de_dag.py

--- src/airflow/de_dag.py
+++ src/airflow/de_dag.py
@@ -5,97 +5,70 @@ from datetime import datetime, timedelta
 import os
 
 
+### 10.2.1 Data Ingestion
 
-srcDir = '/home/ubuntu/stackInsight/s3'
+` cp ~/wiki_bias/src/ingestion/`
+
+`./download.sh`
+
+### 10.2.2 Spark Processing
+
+` cp ~/wiki_bias/src/dataprocessing/`
+
+`/home/ubuntu/wiki_bias/src/dataprocessing/run_articles.sh`
+
+`./edit_history.sh`
+
+### 10.2.3 PageRank Calculation
+` cp ~/wiki_bias/src/pagerank/`
+
+`./page_rank.sh`
+
+### 10.2.4 SQL joins and analytics
+
+`cd $HOME/wiki_bias/src/analytics`
+
+`./run_analytics.sh`
+
+### 10.2.5 Run Flask App
+
+`cd $HOME/wiki_bias/src/flask`
+
+`python wsgi.py`
+
+
+srcDir = '/home/ubuntu/wiki_bias/ingestion'
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2019, 7, 6),
+    'start_date': datetime(2019, 10, 26),
     'retries': 5,
     'retry_delay': timedelta(minutes=1),
 }
 
 dag = DAG(
-    'stackins', default_args=default_args)
+    'wiki_bias', default_args=default_args)
 
 downloadData= BashOperator(
     task_id='download-urls-list',
-    bash_command='python ' + srcDir + 'urls_retrieve.py' ,
+    bash_command='python ' + srcDir + 'generate_text_file.py' ,
     dag=dag)
 loaddatatobucket = BashOperator(
     task_id='load-zip-files',
-    bash_command=   srcDir + 'transfer_to_s3.sh ' ,
+    bash_command=   srcDir + './download.sh' ,
     dag=dag)
 
-downloadData.set_downstream(loaddatatobucket)
-filesconversion = BashOperator(
-     task_id='convert-to-xml',
-     bash_command='python ' + srcDir + 's3_xml.py',
-     dag=dag)
-loaddatatobucket.set_downstream(filesconversion)
+runSpark= BashOperator(
+    task_id='run-spark-articles',
+    bash_command='ssh ubuntu@10.0.0.4 /home/ubuntu/wiki_bias/src/dataprocessing/run_articles.sh',
+    dag=dag)
 
-cmd_posts_pr = "ssh ubuntu@10.0.0.13 /usr/local/spark/bin/spark-submit \
-   --master spark://ip-10-0-0-13:7077 \
-   --deploy-mode cluster \
-   --executor-memory 20G \
-   --driver-memory 8G \
-   --total-executor-cores 4 \
-   --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
-   /home/ubuntu/stackInsights/spark/posts_xml_parq.py"
+runSpark2= BashOperator(
+    task_id='run-spark-edit_history',
+    bash_command='ssh ubuntu@10.0.0.4 /home/ubuntu/wiki_bias/src/dataprocessing/edit_history.sh',
+    dag=dag)
 
-
-posts_p = BashOperator(
-     task_id='posts-parquet-sparks',
-     bash_command=cmd_posts_pr
-     dag=dag)
-posts_p.set_downstream(filesconversion)
-
-
-cmd_posts_pr = "ssh ubuntu@10.0.0.13 /usr/local/spark/bin/spark-submit \
-   --master spark://ip-10-0-0-13:7077 \
-   --deploy-mode cluster \
-   --executor-memory 20G \
-   --driver-memory 8G \
-   --total-executor-cores 4 \
-   --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
-   /home/ubuntu/stackInsights/spark/links_xml_parq.py"
-
-
-links_p = BashOperator(
-     task_id='links-parquet-sparks',
-     bash_command=cmd_links_pr
-     dag=dag)
-links_p.set_downstream(posts_p)
-
-
-
-cmd_pr_spark = "ssh ubuntu@10.0.0.13 /usr/local/spark/bin/spark-submit \
-   --master spark://ip-10-0-0-13:7077 \
-   --deploy-mode cluster \
-   --executor-memory 20G \
-   --total-executor-cores 4 \
-   --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
-   --packages graphframes:graphframes:0.5.0-spark2.1-s_2.1 \
-   /home/ubuntu/stackInsights/spark/pagerank_calculation.py"
-   
-pr_calculation = BashOperator(
-     task_id='page-rank-sparks',
-     bash_command=cmd_pr_spark
-     dag=dag)
-pr_calculation.set_downstream(links_p)  
-   
-
-cmd_df_join = "ssh ubuntu@10.0.0.13 /usr/local/spark/bin/spark-submit \
-   --master spark://ip-10-0-0-13:7077 \
-   --deploy-mode cluster \
-   --executor-memory 20G \
-   --driver-memory 8G \
-   --total-executor-cores 6 \
-   --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
-   /home/ubuntu/stackInsights/spark/join_dfs.py"
-df_join_spark = BashOperator(
-     task_id='join-spark-df',
-     bash_command=cmd_df_join
-     dag=dag)
-df_join_spark.set_downstream(pr_calculation)
-
+sql_commands = BashOperator(
+    task_id='sql-commands',
+    bash_command='ssh ubuntu@10.0.0.14 /home/ubuntu/wiki_bias/src/analytics/edit_history.sh',
+    dag=dag)

f31fa8f344ae37d74bbb67012d4fcb0e5746cabf
Ubuntu <ubuntu@ip-10-0-0-13.ec2.internal>
2019-11-27 23:25:05 +0000

    change file names

--- src/ingestion/download.sh
+++ src/ingestion/download.sh
@@ -1,5 +1,6 @@
 mkdir wikidata
 cd wikidata
 python generate_text_files.py
-wget -i text_file.txt
+wget -i enwiki-pages-mutlistream.txt
+wget -i enwiki-meta-history.txt
 aws s3 sync . s3://wikibuckets/wikidata
